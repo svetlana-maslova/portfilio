100ea86e37bcbfc46131dab7d97502ef0defd98a
Svetlana <svetlenikaya@mail.ru>
2020-07-16 12:45:20 +0500

    add point of pagination

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,33 +1,45 @@
 $(document).ready(function () {
-  const mobileMenuToggle = document.querySelector(".toggle-menu");
-  const mobMenu = document.querySelector(".header-menu");
-  const overlay = document.querySelector('#overlay')
+	// Подключение точек пагинации справа nav-page
+	$('#page-nav').onePageNav({
+		currentClass: 'active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'swing',
+		begin: function () {},
+		end: function () {},
+		scrollChange: function ($currentListItem) {}
+	});
 
-    console.log(mobileMenuToggle)
+	// Мобильное меню header-menu
+	const mobileMenuToggle = document.querySelector(".toggle-menu");
+	const mobMenu = document.querySelector(".header-menu");
+	const overlay = document.querySelector('#overlay')
 
-  mobileMenuToggle.addEventListener("click", function () {
-    mobMenu.classList.toggle("active");
-    this.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
-  window.addEventListener('resize', function(){
-      mobMenu.classList.remove('active');
-      mobileMenuToggle.classList.remove('active');
-      overlay.classList.remove('active');
-  });
+		console.log(mobileMenuToggle)
 
-  //BACK TOP BUTTON
-  $('#backTop').hide();
-  $(window).scroll (function(){
-    if($(this).scrollTop() > 200) {
-      $('#backTop').fadeIn();
-    }
-    else {
-      $('#backTop').fadeOut();
-    }
-  })
+	mobileMenuToggle.addEventListener("click", function () {
+		mobMenu.classList.toggle("active");
+		this.classList.toggle("active");
+		overlay.classList.toggle("active");
+	});
+	window.addEventListener('resize', function(){
+			mobMenu.classList.remove('active');
+			mobileMenuToggle.classList.remove('active');
+			overlay.classList.remove('active');
+	});
 
-
+	// Кнопка прокрутки вверх BACK TOP BUTTON
+	$('#backTop').hide();
+	$(window).scroll (function(){
+		if($(this).scrollTop() > 200) {
+			$('#backTop').fadeIn();
+		}
+		else {
+			$('#backTop').fadeOut();
+		}
+	})
 
 })
 
